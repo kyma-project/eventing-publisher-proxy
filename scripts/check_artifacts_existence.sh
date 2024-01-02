d8ceeb8b035a0ef56f0e375d3b9aba37ba615be4
Carina Kothe <carina.kothe@sap.com>
2024-01-02 11:41:44 +0100

    Add release flow

---
+++ scripts/check_artifacts_existence.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# standard bash error handling
+set -o nounset  # treat unset variables as an error and exit immediately.
+set -o errexit  # exit immediately when a command fails.
+set -E          # must be set if you want the ERR trap
+set -o pipefail # prevents errors in a pipeline from being masked
+
+# This script has the following arguments:
+#                       - Eventing Publisher Proxy binary image tag - mandatory
+#
+# ./check_artifacts_existence.sh 1.1.0 v1.1.0
+
+# Expected variables:
+#             EPP_REPO - eventing-publisher-proxy OCI module image repository
+#             GITHUB_TOKEN - github token
+
+export IMAGE_TAG=$1
+
+PROTOCOL=docker://
+
+if [ $(skopeo list-tags ${PROTOCOL}${EPP_REPO} | jq '.Tags|any(. == env.IMAGE_TAG)') == "true" ]
+then
+  echo "::warning ::Eventing Publisher Proxy binary image for tag ${IMAGE_TAG} already exists"
+else
+  echo "No previous Eventing Publisher Proxy binary image found for tag ${IMAGE_TAG}"
+fi
